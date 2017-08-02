dd763f2840ff76f2d26de595c5ad4abc64170ae3
Jim Vang <vangjim15@gmail.com>
2017-08-02 11:55:05 -0500

    Creating children routes

---
+++ src/containers/Profile.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Profile extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Profile!</h2>
+      </div>
+    ) // end return
+  } // end render
+} // end Template
+
+export default Profile
