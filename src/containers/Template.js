2a2c55fd82b4fcb39522880e133a0add886f3400
Jim Vang <vangjim15@gmail.com>
2017-08-02 11:27:19 -0500

    setting up React Router

---
+++ src/containers/Template.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+class Template extends Component {
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>TicTacTuring</h1>
+        </header>
+        <main>
+          {this.props.children}
+        </main>
+      </div>
+    ) // end return
+  } // end render
+} // end Template
+
+export default Template
