03f884468b345f241945781358067a2c3ef545e3
Jim Vang <vangjim15@gmail.com>
2017-08-10 12:27:10 -0500

    profile page styles

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,12 +1,59 @@
 import React, {Component} from 'react';
+import {Container, Name, GameListHeader, GameList, GameRecord, Column, ColumnLabels} from '../styled/Profile';
 
 class Profile extends Component {
 
+  // dumbie data
+  static defaultProps = {
+    user: {
+      email: 'USER_EMAIL',
+      games: [
+        {
+          winner: true,
+          createdAt: '8/7/17',
+          id: '0001'
+        },
+        {
+          winner: true,
+          createdAt: '8/8/17',
+          id: '0002'
+        },
+        {
+          winner: true,
+          createdAt: '8/9/17',
+          id: '0003'
+        },
+      ] // end game
+    } // end user
+  } // end static defaultProps
+
   render() {
+    let {email} = this.props.user
     return (
-      <div>
-        <h2>Profile!</h2>
-      </div>
+      <Container>
+        <Name>
+          {email}
+        </Name>
+        <GameList>
+          <GameListHeader>
+            MyGames
+          </GameListHeader>
+          <ColumnLabels>
+            <Column>
+              Outcome
+            </Column>
+            <Column>
+              Guess
+            </Column>
+            <Column>
+              Guessed Correctly
+            </Column>
+            <Column>
+              Date
+            </Column>
+          </ColumnLabels>
+        </GameList>
+      </Container>
     ) // end return
   } // end render
 } // end Template
